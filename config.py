b632374f29d51698961bbc1042e4aa946ca7ca2d
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-06-23 23:46:13 +0530

    χqυєєη

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ASIFXQUEEN/XQUEENRADHE903",
+    "https://github.com/ASIFXQUEEN903/RADHEXMUSICBYXQUEEN903",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
